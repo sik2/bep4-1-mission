a68b56e07338521b18d55916b5e99fe4342f7bb2
sik2 <bickwang123@gmail.com>
2025-12-23 14:35:25 +0900

    0004

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -30,6 +30,8 @@ public class Post extends BaseIdAndTime {
 
         comments.add(postComment);
 
+        author.increaseActivityScore(1);
+
         return postComment;
     }
 
--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -18,6 +18,9 @@ public class PostService {
     }
 
     public Post write(Member author, String title, String content) {
+
+        author.increaseActivityScore(3);
+
         return postRepository.save(new Post(author, title, content));
     }
 
