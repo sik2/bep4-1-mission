5307bf76402725857c1b48328281e7480a64761b
sik2 <bickwang123@gmail.com>
2025-12-26 16:41:26 +0900

    0029

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,8 +1,6 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
-import com.back.boundedContext.member.domain.MemberPolicy;
-import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.RsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,18 +11,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class MemberFacade {
-    private final MemberRepository memberRepository;
+    private final MemberSupport memberSupport;
     private final MemberJoinUseCase memberJoinUseCase;
-    private final MemberPolicy memberPolicy;
+    private final MemberGetRandomSecureTipUseCase memberGetRandomSecureTipUseCase;
 
     @Transactional(readOnly = true)
     public long count() {
-        return memberRepository.count();
+        return memberSupport.count();
     }
 
     @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberSupport.findByUsername(username);
     }
 
     @Transactional
@@ -34,11 +32,10 @@ public class MemberFacade {
 
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
-        return memberRepository.findById(id);
+        return memberSupport.findById(id);
     }
 
     public String getRandomSecureTip() {
-        return "비밀번호의 유효기간은 %d일 입니다."
-                .formatted(memberPolicy.getNeedToChangePasswordDays());
+        return memberGetRandomSecureTipUseCase.getRandomSecureTip();
     }
 }
