e0c9fe00197f4e2c1f646ff8cca27e62c594bd5c
sik2 <bickwang123@gmail.com>
2025-12-26 12:18:26 +0900

    0024

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -1,7 +1,9 @@
 package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.Wallet;
 import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.boundedContext.cash.out.WalletRepository;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CashFacade {
     private final CashMemberRepository cashMemberRepository;
+    private final WalletRepository walletRepository;
 
     @Transactional
     public CashMember syncMember(MemberDto member) {
@@ -26,5 +29,12 @@ public class CashFacade {
         );
 
         return cashMemberRepository.save(_member);
+    }
+
+    @Transactional
+    public Wallet createWallet(CashMember holder) {
+        Wallet wallet = new Wallet(holder);
+
+        return walletRepository.save(wallet);
     }
 }
