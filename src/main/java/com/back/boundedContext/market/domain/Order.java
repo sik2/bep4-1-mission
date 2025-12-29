dc98b2aa8f30dbdebf770dda54f25dbaca3bafa4
sik2 <bickwang123@gmail.com>
2025-12-29 16:04:50 +0900

    0036

--- src/main/java/com/back/boundedContext/market/domain/Order.java
+++ src/main/java/com/back/boundedContext/market/domain/Order.java
@@ -27,6 +27,7 @@ public class Order extends BaseIdAndTime {
     private MarketMember buyer;
     private long price;
     private long salePrice;
+    private LocalDateTime cancelDate;
     private LocalDateTime requestPaymentDate;
     private LocalDateTime paymentDate;
 
@@ -78,5 +79,13 @@ public class Order extends BaseIdAndTime {
 
     public void cancelRequestPayment() {
         requestPaymentDate = null;
+    }
+
+    public boolean isCanceled() {
+        return cancelDate != null;
+    }
+
+    public boolean isPaymentInProgress() {
+        return requestPaymentDate != null && paymentDate == null && cancelDate == null;
     }
 }
