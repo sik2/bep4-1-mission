f87fc94c59395fe747d04600c8bcb8f13ebc5b63
sik2 <bickwang123@gmail.com>
2025-12-29 12:21:20 +0900

    0034

--- src/main/java/com/back/boundedContext/market/app/MarketSupport.java
+++ src/main/java/com/back/boundedContext/market/app/MarketSupport.java
@@ -6,6 +6,7 @@ import com.back.boundedContext.market.domain.MarketMember;
 import com.back.boundedContext.market.domain.Product;
 import com.back.boundedContext.market.out.CartRepository;
 import com.back.boundedContext.market.out.MarketMemberRepository;
+import com.back.boundedContext.market.out.OrderRepository;
 import com.back.boundedContext.market.out.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,7 @@ public class MarketSupport {
     private final ProductRepository productRepository;
     private final MarketMemberRepository marketMemberRepository;
     private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
 
     public long countProducts() {
         return productRepository.count();
@@ -34,5 +36,9 @@ public class MarketSupport {
 
     public Optional<Product> findProductById(int id) {
         return productRepository.findById(id);
+    }
+
+    public long countOrders() {
+        return orderRepository.count();
     }
 }
---
+++ src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
@@ -0,0 +1,6 @@
+package com.back.boundedContext.market.domain;
+
+
+public class MarketPolicy {
+    public static final double PRODUCT_PAYOUT_RATE = 90;
+}
