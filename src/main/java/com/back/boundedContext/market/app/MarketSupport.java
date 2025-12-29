938cbfa2d6a072468d77f9864819e08ad45a2691
sik2 <bickwang123@gmail.com>
2025-12-29 11:31:52 +0900

    0033

--- src/main/java/com/back/boundedContext/market/app/MarketSupport.java
+++ src/main/java/com/back/boundedContext/market/app/MarketSupport.java
@@ -1,20 +1,24 @@
 package com.back.boundedContext.market.app;
 
 
+import com.back.boundedContext.market.domain.Cart;
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.market.domain.Product;
+import com.back.boundedContext.market.out.CartRepository;
 import com.back.boundedContext.market.out.MarketMemberRepository;
 import com.back.boundedContext.market.out.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class MarketSupport {
     private final ProductRepository productRepository;
     private final MarketMemberRepository marketMemberRepository;
+    private final CartRepository cartRepository;
 
     public long countProducts() {
         return productRepository.count();
@@ -22,5 +26,13 @@ public class MarketSupport {
 
     public Optional<MarketMember> findMemberByUsername(String username) {
         return marketMemberRepository.findByUsername(username);
+    }
+
+    public Optional<Cart> findCartByBuyer(MarketMember buyer) {
+        return cartRepository.findByBuyer(buyer);
+    }
+
+    public Optional<Product> findProductById(int id) {
+        return productRepository.findById(id);
     }
 }
